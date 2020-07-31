8db5b8994c1a27cc7dac67360e797144169c00b3
young917 <choecindy05@gmail.com>
2020-07-31 22:39:15 +0900

    debug code : convert edge label into node label

--- nas_201_api/model_spec_201.py
+++ nas_201_api/model_spec_201.py
@@ -6,151 +6,160 @@ import numpy as np
 
 # Graphviz is optional and only required for visualization.
 try:
-	import graphviz	 # pylint: disable=g-import-not-at-top
+    import graphviz     # pylint: disable=g-import-not-at-top
 except ImportError:
-	pass
+    pass
 
 
 class ModelSpec(object):
-	"""Model specification given adjacency matrix and labeling."""
+    """Model specification given adjacency matrix and labeling."""
     
-	def __init__(self, index, model_str, data_format='channels_last'):
-		"""Initialize the module spec.
+    def __init__(self, index, model_str, data_format='channels_last'):
+        """Initialize the module spec.
 
-		Args:
-			matrix: ndarray or nested list with shape [V, V] for the adjacency matrix.
-			ops: V-length list of labels for the base ops used. The first and last
-				elements are ignored because they are the input and output vertices
-				which have no operations. The elements are retained to keep consistent
-				indexing.
-			data_format: channels_last or channels_first.
+        Args:
+            matrix: ndarray or nested list with shape [V, V] for the adjacency matrix.
+            ops: V-length list of labels for the base ops used. The first and last
+                elements are ignored because they are the input and output vertices
+                which have no operations. The elements are retained to keep consistent
+                indexing.
+            data_format: channels_last or channels_first.
 
-		Raises:
-			ValueError: invalid matrix or ops
-		"""
-		self.index = index
-		self.model_str = model_str
-		self.matrix, self.ops = self._convert_to_matrix_ops(model_str)
-		self.valid_spec = True
-		self.search_space = [ 'input', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3', 'output']
-		self._prune()
+        Raises:
+            ValueError: invalid matrix or ops
+        """
+        self.index = index
+        self.model_str = model_str
+        self.matrix, self.ops = self._convert_to_matrix_ops(model_str)
+        self.valid_spec = True
+        self.search_space = ['skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
+        self._prune()
 
-		self.data_format = data_format
+        self.data_format = data_format
         
-		if not isinstance(matrix, np.ndarray):
-			matrix = np.array(matrix)
-		shape = np.shape(matrix)
-		if len(shape) != 2 or shape[0] != shape[1]:
-			raise ValueError('matrix must be square')
-		if shape[0] != len(ops):
-			raise ValueError('length of ops must match matrix dimensions')
-		if not is_upper_triangular(matrix):
-			raise ValueError('matrix must be upper triangular')
+        if not isinstance(matrix, np.ndarray):
+            matrix = np.array(matrix)
+        shape = np.shape(matrix)
+        if len(shape) != 2 or shape[0] != shape[1]:
+            raise ValueError('matrix must be square')
+        if shape[0] != len(ops):
+            raise ValueError('length of ops must match matrix dimensions')
+        if not is_upper_triangular(matrix):
+            raise ValueError('matrix must be upper triangular')
     
-	def _convert_to_matrix_ops(self, model_str):
-		search_space = [ 'none', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
-		node_strs = arch_str.split('+')
-		num_nodes = len(node_strs) + 1
-		org_end_node = len(node_strs)
-		matrix = np.zeros((num_nodes, num_nodes))
-        
-		converted_nodes = 1;
-		to_node_dict = defaultdict(list)
-		converted_ops = ['input']
-        
-		for i, node_str in enumerate(node_strs):
-			inputs = list(filter(lambda x: x != '', node_str.split('|')))
-			for xinput in inputs: assert len(xinput.split('~')) == 2, 'invalid input length : {:}'.format(xinput)
-			for xi in inputs:
-				op, idx = xi.split('~')
-				if op not in self.search_space: raise ValueError('this op ({:}) is not in {:}'.format(op, self.search_space))
-				op_idx, node_idx = self.search_space.index(op), int(idx)
-				matrix[i+1, node_idx] = op_idx
-				if op_idx != 0:
-					to_node_dict[i+1].append(converted_nodes)
-					converted_ops.append(op)
-					converted_nodes += 1
-                    
-		converted_ops.append('output')
-		converted_nodes += 1      
-		converted_end_node = converted_nodes - 1
-		converted_matrix = np.zeros((converted_nodes, converted_nodes))
-		for i in range(1, matrix.shape[0]):
-			turn = 0
-			for j in range(i):
-				if matrix[i][j] != 0:
-					for k in to_node_dict[j]:
-						converted_matrix[j][to_node_dict[i][turn]] = 1
-						converted_matrix[to_node_dict[i][turn]][j] = 1
-						turn += 1
-		for k in to_node_dict[org_end_node]:
-			converted_matrix[k][converted_end_node] = 1
-			converted_matrix[converted_end_node][k] = 1
-            
-		return converted_matrix, converted_ops          
+    def convert_to_matrix_ops(arch_str):
+        search_space = [ 'none', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
+        node_strs = arch_str.split('+')
+        num_nodes = len(node_strs) + 1
+        org_end_node = []
+        matrix = np.zeros((num_nodes, num_nodes))
 
+        converted_nodes = 1;
+        to_node_dict = defaultdict(list)
+        to_node_dict[0] = [0]
+        converted_ops = ['input']
 
-	def _prune(self):
-		"""Prune the extraneous parts of the graph.
+        for i, node_str in enumerate(node_strs):
+            inputs = list(filter(lambda x: x != '', node_str.split('|')))
+            for xinput in inputs: assert len(xinput.split('~')) == 2, 'invalid input length : {:}'.format(xinput)
+            for xi in inputs:
+                op, idx = xi.split('~')
+                if op not in search_space: raise ValueError('this op ({:}) is not in {:}'.format(op, self.search_space))
+                op_idx, node_idx = search_space.index(op), int(idx)
+                matrix[i+1, node_idx] = op_idx
+                if op_idx != 0:
+                    to_node_dict[i+1].append(converted_nodes)
+                    converted_ops.append(op)
+                    converted_nodes += 1
+            if len(to_node_dict[i+1]) == 0 :
+                to_node_dict[i+1].append(0)            
 
-		General procedure:
-			1) Remove parts of graph not connected to input.
-			2) Remove parts of graph not connected to output.
-			3) Reorder the vertices so that they are consecutive after steps 1 and 2.
+        for i in range(num_nodes):
+            if np.count_nonzero(matrix[:,i]) == 0:
+                org_end_node.append(i) 
 
-		These 3 steps can be combined by deleting the rows and columns of the
-		vertices that are not reachable from both the input and output (in reverse).
-		"""
-		num_vertices = np.shape(self.original_matrix)[0]
+        converted_ops.append('output')
+        converted_nodes += 1      
+        converted_end_node = converted_nodes - 1
+        converted_matrix = np.zeros((converted_nodes, converted_nodes))
+        for i in range(1, matrix.shape[0]):
+            turn = 0
+            for j in range(i):
+                if matrix[i][j] != 0:
+                    to_node = to_node_dict[i][turn]
+                    for k in to_node_dict[j]:
+                        converted_matrix[k][to_node] = 1
+                        converted_matrix[to_node][k] = 1
+                    turn += 1
+        print(org_end_node)
+        for end_node in org_end_node:         
+            for k in to_node_dict[end_node]:
+                converted_matrix[k][converted_end_node] = 1
+                converted_matrix[converted_end_node][k] = 1
 
-		# DFS forward from input
-		visited_from_input = set([0])
-		frontier = [0]
-		while frontier:
-			top = frontier.pop()
-			for v in range(top + 1, num_vertices):
-				if self.original_matrix[top, v] and v not in visited_from_input:
-					visited_from_input.add(v)
-					frontier.append(v)
+        return converted_matrix, converted_ops
 
-		# DFS backward from output
-		visited_from_output = set([num_vertices - 1])
-		frontier = [num_vertices - 1]
-		while frontier:
-			top = frontier.pop()
-			for v in range(0, top):
-				if self.original_matrix[v, top] and v not in visited_from_output:
-					visited_from_output.add(v)
-					frontier.append(v)
+    def _prune(self):
+        """Prune the extraneous parts of the graph.
 
-		# Any vertex that isn't connected to both input and output is extraneous to
-		# the computation graph.
-		extraneous = set(range(num_vertices)).difference(
-				visited_from_input.intersection(visited_from_output))
+        General procedure:
+            1) Remove parts of graph not connected to input.
+            2) Remove parts of graph not connected to output.
+            3) Reorder the vertices so that they are consecutive after steps 1 and 2.
 
-		# If the non-extraneous graph is less than 2 vertices, the input is not
-		# connected to the output and the spec is invalid.
-		if len(extraneous) > num_vertices - 2:
-			self.matrix = None
-			self.ops = None
-			self.valid_spec = False
-			return
+        These 3 steps can be combined by deleting the rows and columns of the
+        vertices that are not reachable from both the input and output (in reverse).
+        """
+        num_vertices = np.shape(self.original_matrix)[0]
 
-		self.matrix = np.delete(self.matrix, list(extraneous), axis=0)
-		self.matrix = np.delete(self.matrix, list(extraneous), axis=1)
-		for index in sorted(extraneous, reverse=True):
-			del self.ops[index]
+        # DFS forward from input
+        visited_from_input = set([0])
+        frontier = [0]
+        while frontier:
+            top = frontier.pop()
+            for v in range(top + 1, num_vertices):
+                if self.original_matrix[top, v] and v not in visited_from_input:
+                    visited_from_input.add(v)
+                    frontier.append(v)
 
-	def hash_spec(self):
-		"""Computes the isomorphism-invariant graph hash of this spec.
+        # DFS backward from output
+        visited_from_output = set([num_vertices - 1])
+        frontier = [num_vertices - 1]
+        while frontier:
+            top = frontier.pop()
+            for v in range(0, top):
+                if self.original_matrix[v, top] and v not in visited_from_output:
+                    visited_from_output.add(v)
+                    frontier.append(v)
 
-		Args:
-			canonical_ops: list of operations in the canonical ordering which they
-				were assigned (i.e. the order provided in the config['available_ops']).
+        # Any vertex that isn't connected to both input and output is extraneous to
+        # the computation graph.
+        extraneous = set(range(num_vertices)).difference(
+                visited_from_input.intersection(visited_from_output))
 
-		Returns:
-			MD5 hash of this spec which can be used to query the dataset.
-		"""
-		# Invert the operations back to integer label indices used in graph gen.
-		labeling = [-1] + [self.search_space.index(op) for op in self.ops[1:-1]] + [-2]
-		return graph_util.hash_module(self.matrix, labeling)
+        # If the non-extraneous graph is less than 2 vertices, the input is not
+        # connected to the output and the spec is invalid.
+        if len(extraneous) > num_vertices - 2:
+            self.matrix = None
+            self.ops = None
+            self.valid_spec = False
+            return
+
+        self.matrix = np.delete(self.matrix, list(extraneous), axis=0)
+        self.matrix = np.delete(self.matrix, list(extraneous), axis=1)
+        for index in sorted(extraneous, reverse=True):
+            del self.ops[index]
+
+    def hash_spec(self):
+        """Computes the isomorphism-invariant graph hash of this spec.
+
+        Args:
+            canonical_ops: list of operations in the canonical ordering which they
+                were assigned (i.e. the order provided in the config['available_ops']).
+
+        Returns:
+            MD5 hash of this spec which can be used to query the dataset.
+        """
+        # Invert the operations back to integer label indices used in graph gen.
+        labeling = [-1] + [self.search_space.index(op) for op in self.ops[1:-1]] + [-2]
+        return graph_util.hash_module(self.matrix, labeling)
