de00731d6a3da412a2ae8eaaf335e62768290122
Minyoung Choe <choecindy@naver.com>
2020-12-11 12:50:28 +0900

    make code clean

--- nas_201_api/api_201.py
+++ nas_201_api/api_201.py
@@ -50,55 +50,77 @@ class NASBench201API(object):
         # ex)'|avg_pool_3x3~0|+|nor_conv_1x1~0|skip_connect~1|+|nor_conv_1x1~0|skip_connect~1|skip_connect~2|'
         self.meta_archs = copy.deepcopy( path['meta_archs'] )
 
-        # Stores the statistics that are computed via training and evaluating the
-        # model on cifar10, cifar100, ImageNet16. Statistics are computed for multiple repeats of each
-        # model at each max epoch length.
-        # hash --> hps --> all_results --> (data name, seed) --> metric name --> scalar
-        self.hash2results = {}
-
+        # Stores the statistics that are computed via training and evaluating the model on cifar10, cifar100, ImageNet16. 
+        # Statistics are computed for multiple repeats of each model at each max epoch length.
+        # hash --> hash_value
+        #      --> arch_str
+        #      --> dataset_seed --> data name : [seed1, seed2, ... ]
+        #      --> all_results  --> (data name, seed) --> metric name --> scalar
         # Stores architecture string
         # cf)   matrix, ops -> modelspec                     -> hash --> hash2archstr -> get arch str 
         # cf)   arch str    -> modelspec(get matrix and ops) -> hash --> hash2results -> get statistics
-        self.hash2archstr = {}
+        self.hash2infos = path['arch2infos']
 
-        self._avaliable_hps = set(['12', '200'])
-        self._avaliable_epochkeys = ['less', 'full']
+        self._avaliable_epochs = ['less': 20, 'full': 200]
         self._avaliable_datasets = ['cifar10-valid', 'cifar10', 'cifar100', 'ImageNet16-120']
-        self.evaluated_indexes = sorted(list(path['evaluated_indexes']))
+        self.allhashvalues = list(path['arch2infos'].keys())
         self.search_space = ['skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
         self.total_time = 0
         self.total_epochs = 0 # not implement yet
 
         max_nodes=0
         max_edges=0
-        for archidx in sorted(list(path['arch2infos'].keys())):
-            arch_str = path['arch2infos'][archidx]['full']['arch_str']
+        for hash_val in sorted(list()):
+            arch_str = path['arch2infos'][hash_val]['arch_str']
             assert arch_str in self.meta_archs, 'This [{:}]-th arch not in the meta_archs. This arch is {:}.'.format(archidx, arch_str)
-            assert arch_str == path['arch2infos'][archidx]['less']['arch_str'], "Not match 'less' and 'full' arch_str"
-
+            
             modelspec = ModelSpec(model_str=arch_str)
             max_nodes = max(max_nodes, modelspec.num_nodes)
             max_edges = max(max_edges, modelspec.num_edges)
-            hash_val = modelspec.hash_spec(self.search_space)
-            self.hash2results[hash_val] = path['arch2infos'][archidx]
-            self.hash2archstr[hash_val] = arch_str
-
         self.max_edges = max_edges
         self.max_nodes = max_nodes
     
     def hash_iterator(self):
         """Returns iterator over all unique model hashes."""
-        return self.hash2archstr.keys()
+        return self.allhashvalues
     
-    def get_modelspec_by_hash(self, hash_val):
-        """Return modelspec by hash value"""
-        archstr = self.hash2archstr[hash_val]
-        arch = ModelSpec(model_str=archstr)
-        return arch
-
     def get_modelspec(self, matrix, ops):
         """Return model spec."""        
         return ModelSpec(matrix=matrix, ops=ops)
+    
+    def get_modelspec_by_hash(self, hash_val):
+        """Return modelspec by hash value"""
+        archstr = self.hash2infos[hash_val]['arch_str']
+        arch = ModelSpec(model_str=archstr)
+        return arch
+    
+    def get_budget_counters(self):
+        """Returns the time and budget counters."""
+        return self.total_time, self.total_epochs
+
+    def _check_spec(self, model_spec):
+        """Checks that the model spec is within the dataset."""
+        if not model_spec.valid_spec:
+            raise OutOfDomainError('invalid spec, provided graph is disconnected.')
+        
+        model_hash = model_spec.hash_spec(self.search_space)
+        if model_hash not in self.allhashvalues:
+            raise OutOfDomainError('unsupported model hash %s ' % (model_hash))
+
+        num_vertices = len(model_spec.ops)
+        num_edges = np.sum(model_spec.matrix)
+        if num_vertices > self.max_nodes:
+            raise OutOfDomainError('too many vertices, got %d (max vertices = %d)' % (num_vertices, self.max_nodes))
+        if num_edges > self.max_edges:
+            raise OutOfDomainError('too many edges, got %d (max edges = %d)' % (num_edges, self.max_edges))
+
+        if model_spec.ops[0] != 'input':
+            raise OutOfDomainError('first operation should be \'input\'')
+        if model_spec.ops[-1] != 'output':
+            raise OutOfDomainError('last operation should be \'output\'')
+        for op in model_spec.ops[1:-1]:
+            if op not in self.search_space:
+                raise OutOfDomainError('unsupported op %s (available ops = %s)' % (op, self.search_space))
     
     def is_valid(self, model_spec):
         """Checks the validity of the model_spec."""
@@ -109,44 +131,10 @@ class NASBench201API(object):
         
         return True
     
-    def _check_spec(self, model_spec):
-        """Checks that the model spec is within the dataset."""
-        if not model_spec.valid_spec:
-            raise OutOfDomainError('invalid spec, provided graph is disconnected.')
-        
-        model_hash = model_spec.hash_spec(self.search_space)
-        if model_hash not in self.hash2results.keys():
-            raise OutOfDomainError('unsupported model hash %s ' % (model_hash))
-
-        num_vertices = len(model_spec.ops)
-        num_edges = np.sum(model_spec.matrix)
-
-        if num_vertices > self.max_nodes:
-            raise OutOfDomainError('too many vertices, got %d (max vertices = %d)'
-                                                         % (num_vertices, self.max_nodes))
-
-        if num_edges > self.max_edges:
-            raise OutOfDomainError('too many edges, got %d (max edges = %d)'
-                                                         % (num_edges, self.max_edges))
-
-        if model_spec.ops[0] != 'input':
-            raise OutOfDomainError('first operation should be \'input\'')
-        if model_spec.ops[-1] != 'output':
-            raise OutOfDomainError('last operation should be \'output\'')
-        for op in model_spec.ops[1:-1]:
-            if op not in self.search_space:
-                raise OutOfDomainError('unsupported op %s (available ops = %s)'
-                                                             % (op, self.search_space))
-                
-    def get_budget_counters(self):
-        """Returns the time and budget counters."""
-        return self.total_time, self.total_epochs
-    
     def query(self, modelspec, option, dataset='cifar10-valid', epochs='less'):
         """Fetch one of the evaluations for this model spec
         * option == 'valid'
-        Fetch one of the evaluations for this model spec.
-        sample one of the config['num_repeats'] evaluations of the model.
+        Fetch one of the evaluations for this model spec by sampling one seed from seed_set.
         increment the budget counters for benchmarking purposes.
         
         * option == 'test'
@@ -165,20 +153,21 @@ class NASBench201API(object):
         """
         assert option == 'valid' or option == 'test'
         assert dataset in self._avaliable_datasets
-        assert epochs in self._avaliable_epochkeys
+        assert epochs in self._avaliable_epochs.keys()
 
         if option == 'valid':
             model_hash = modelspec.hash_spec(self.search_space)
-            archresult = self.hash2results[model_hash][epochs]['all_results']
-            seeds = self.hash2results[model_hash][epochs]['dataset_seed'][dataset]
+            archresult = self.hash2infos[model_hash]['all_results'][epochs]
+            seeds = self.hash2infos[model_hash]['dataset_seed'][epochs][dataset]
             picked_seed = random.choice(seeds)
             info = archresult[(dataset, picked_seed)]
             eval_name = [n for n in info['eval_names'] if 'valid' in n][0]
             self.total_time += (info['train_times'] + info['eval_times'][eval_name])
+            self.total_epochs += self._available_epochs[epochs]            
             return info['eval_acc1es'][eval_name]        
         elif option == 'test':
-            archresult = self.hash2results[model_hash][epochs]['all_results']
-            seeds = self.hash2results[model_hash][epochs]['dataset_seed'][dataset]
+            archresult = self.hash2infos[model_hash]['all_results'][epochs]
+            seeds = self.hash2results[model_hash]['dataset_seed'][epochs][dataset]
             avg_test_acc = 0
             for seed in seeds:
                 info = archresult[(dataset, seed)]
